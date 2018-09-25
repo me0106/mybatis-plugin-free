916756d9c61200a877e6d3b8574e20b0e96cc961
nan-lou <726758103@qq.com>
2018-09-26 01:14:19 +0800

    -

---
+++ src/main/java/me/nanlou/param/psi/ParamMethod.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package me.nanlou.param.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface ParamMethod extends PsiElement {
+
+  @NotNull
+  ParamClassName getClassName();
+
+  @NotNull
+  ParamMethodName getMethodName();
+
+  @Nullable
+  ParamParamList getParamList();
+
+}
